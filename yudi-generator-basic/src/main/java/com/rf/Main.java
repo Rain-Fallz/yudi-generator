19f322b5ac0a2369e43444236af2e12db1809c1c
Allen <935870076@qq.com>
2024-03-07 22:46:11 +0800

    feat(all): 项目初始化

---
+++ yudi-generator-basic/src/main/java/com/rf/Main.java
@@ -0,0 +1,11 @@
+package com.rf;
+
+/**
+ * @author rainfall
+ * @date ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
